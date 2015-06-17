871033dc4f007f67427fd3b93dcffe12d8d8338e
Ilya Belyaichev <belyaichev@fosslabs.ru>
2015-06-17 10:55:23 +0300

    first

--- src/main/java/com/beleychev/JdbcSpitterDAO.java
+++ src/main/java/com/beleychev/JdbcSpitterDAO.java
@@ -1,31 +1,54 @@
 package com.beleychev;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import java.util.List;
 
 /**
  * Created by ilya on 17.06.15.
  */
-public class JdbcSpitterDAO implements SpitterDAO{
-
-    public int getID() {
-        return 0;
-    }
-
-    public int getAge() {
-        return 0;
-    }
-
-    public String getName() {
-        return null;
-    }
-
-    public String getEmail() {
-        return null;
-    }
+public class JdbcSpitterDAO extends JdbcDaoSupport implements SpitterDAO{
 
     private JdbcTemplate jdbcTemplate;
 
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public void addSpitter(Spitter spitter) {
+
+    }
+
+    public void saveSpitter(Spitter spitter) {
+
+    }
+
+    public Spitter getSpitterById(long id) {
+        return null;
+    }
+
+    public List<Spittle> getRecentSpittle() {
+        return null;
+    }
+
+    public void saveSpittle(Spittle spittle) {
+
+    }
+
+    public List<Spittle> getSpittlesForSpitter(Spitter spitter) {
+        return null;
+    }
+
+    public Spitter getSpitterByUsername(String username) {
+        return null;
+    }
+
+    public void deleteSpittle(long id) {
+
+    }
+
+    public Spittle getSpittleById(long id) {
+        return null;
+    }
+
+    public List<Spitter> findAllSpitters() {
+        return null;
     }
 }
--- src/main/java/com/beleychev/SpitterDAO.java
+++ src/main/java/com/beleychev/SpitterDAO.java
@@ -1,11 +1,28 @@
 package com.beleychev;
 
+import java.util.List;
+
 /**
  * Created by ilya on 17.06.15.
  */
 public interface SpitterDAO {
-    int getID();
-    int getAge();
-    String getName();
-    String getEmail();
+    void addSpitter(Spitter spitter);
+
+    void saveSpitter(Spitter spitter);
+
+    Spitter getSpitterById(long id);
+
+    List<Spittle> getRecentSpittle();
+
+    void saveSpittle(Spittle spittle);
+
+    List<Spittle> getSpittlesForSpitter(Spitter spitter);
+
+    Spitter getSpitterByUsername(String username);
+
+    void deleteSpittle(long id);
+
+    Spittle getSpittleById(long id);
+
+    List<Spitter> findAllSpitters();
 }
