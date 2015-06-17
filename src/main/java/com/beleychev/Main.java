6f1017a27895c3654ba5d125a4188567a1bcc2a5
kterrita <kterrita@gmail.com>
2015-06-17 08:19:50 +0300

    Initial commit

---
+++ src/main/java/com/beleychev/Main.java
@@ -0,0 +1,71 @@
+package com.beleychev;
+
+import java.sql.*;
+
+/**
+ * Created by ilya on 16.06.2015.
+ */
+public class Main {
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/mydbtest";
+
+    //  Database credentials
+    static final String USER = "root";
+    static final String PASS = "root";
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+
+        try {
+            Class.forName(JDBC_DRIVER);
+
+            System.out.println("Connection...");
+            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            System.out.println("Creating statement...");
+            statement = connection.createStatement();
+            String sql = "SELECT id, name, age, email FROM Student";
+            ResultSet rs = statement.executeQuery(sql);
+
+            //Извлечение данных
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int age = rs.getInt("age");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+
+                //Отображение извлеченных данных
+                System.out.print("ID: " + id);
+                System.out.print(", Age: " + age);
+                System.out.print(", Name: " + name);
+                System.out.println(", Email: " + email);
+            }
+
+            rs.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("It works!");
+    }
+}
---
+++ src/main/resources/q.sql
@@ -0,0 +1,2 @@
+#insert into student(name, age, email) values('ilya', 22, 'kterrita@gmail.com')
+#update student set name='Yulia', age=21 where id=2
